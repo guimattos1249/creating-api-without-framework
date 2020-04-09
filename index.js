a41f3aeae77691077d5c36165ebf81ead2bd0ac1
Guilherme Mattos <guilhermevsmattos@outlook.com>
2020-04-09 10:51:15 -0300

    creating a server without framework

---
+++ index.js
@@ -0,0 +1,10 @@
+var http = require('http');
+
+http.createServer(
+    function (req, res) {
+        res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+        res.end('<h2>Olá! Meu WebServer está no ar!</h2>');
+    }
+).listen(3000);
+
+console.log('Servidor iniciado.');
