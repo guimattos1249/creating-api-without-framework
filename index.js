10ac0ac7c9ecc02f750a60e939693ce44505af66
Guilherme Mattos <guilhermevsmattos@outlook.com>
2020-04-09 11:14:33 -0300

    Created a simple exemple of routes

--- index.js
+++ index.js
@@ -3,7 +3,18 @@ var http = require('http');
 http.createServer(
     function (req, res) {
         res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-        res.end('<h2>Olá! Meu WebServer está no ar!</h2>');
+        switch (req.url) {
+            case '/':
+                res.write('<h2>Bem vindo a home page</h2>');
+                break;
+            case '/user': 
+                res.write('<h2>Seja bem vindo a página de usuários</h2>');
+                break;
+            default:
+                res.write('<h2>Página não encontrada!</h2>');
+                break;
+        }
+        res.end();
     }
 ).listen(3000);
 
