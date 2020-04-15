8d2fff166261318d39aa05d61e881d0ef4f26878
Guilherme Mattos <guilhermevsmattos@outlook.com>
2020-04-15 20:58:46 -0300

    creating a server witout framework using query string

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var http = require('http');
 
-http.createServer(
+var server = http.createServer(
     function (req, res) {
         res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
         switch (req.url) {
